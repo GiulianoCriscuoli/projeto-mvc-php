58eab627ea168fc928d7f3068e766117b9da8d62
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-09-23 11:57:53 -0300

    incluindo bootstrap no projeto

---
+++ app/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+// define é uma constante, que pode ser utilizada por todo o sistema
+define('APP', dirname(__FILE__)); //dirname retorna o diretório atual
+define('URL', 'http://localhost/php7/projetomvc/');
+define('APP_NAME', 'Meu sistema MVC');
+
+const APP_VERSION = '1.0.0';
+
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+include './../app/conf.php';
 include './../app/Libraries/Router.php';
 include './../app/Libraries/Controller.php';
 
@@ -10,15 +10,20 @@ include './../app/Libraries/Controller.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MVC</title>
+    <title><?php echo APP_NAME; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href= "<?php echo URL ?>/public/css/style.css"/>
 </head>
 <body>
 
     <?php
         $routes = new Router();
-        
-      
+    
     ?>
     
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="<?php URL; ?>/public/js/jquery.functions.js"> </script>
 </body>
 </html>
